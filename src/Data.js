b77c97a5041fd2971d6a820dd3a0244f788c97f8
akshara1118 <74053855+akshara1118@users.noreply.github.com>
2021-02-05 19:09:31 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,135 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Apple",
+        img: "imgs/apple.png",
+        price: 2,
+        company: "India-Mart",
+        info: 
+        "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Mango",
+        img: "imgs/mango.jpg",
+        price: 5,
+        company: "Metro",
+        info: 
+        "A mango is a stone fruit produced from numerous species of tropical trees belonging to the flowering plant genus Mangifera, cultivated mostly for their edible fruit. Most of these species are found in nature as wild mangoes. The genus belongs to the cashew family Anacardiaceae. Mangoes are native to South Asia, from where the common mango or Indian mango, Mangifera indica, has been distributed worldwide to become one of the most widely cultivated fruits in the tropics. Other Mangifera species (e.g. horse mango, Mangifera foetida) are grown on a more localized basis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Orange",
+        img: "imgs/orange.jpg",
+        price: 2,
+        company: "Kompass",
+        info: 
+        "The orange is the fruit of various citrus species in the family Rutaceae (see list of plants known as orange); it primarily refers to Citrus × sinensis, which is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange. The sweet orange reproduces asexually (apomixis through nucellar embryony); varieties of sweet orange arise through mutations.The orange is a hybrid between pomelo (Citrus maxima) and mandarin (Citrus reticulata).The chloroplast genome, and therefore the maternal line, is that of pomelo.The sweet orange has had its full genome sequenced.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Kiwi",
+        img: "imgs/kiwi.jpg",
+        price: 7,
+        company: "India-Mart",
+        info: 
+        "Kiwis are small fruits that pack a lot of flavor and plenty of health benefits. Their green flesh is sweet and tangy. It’s also full of nutrients like vitamin C, vitamin K, vitamin E, folate, and potassium. They also have a lot of antioxidants and are a good source of fiber. Their small black seeds are edible, as is the fuzzy brown peel, though many prefer to peel the kiwi before eating it.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Banana",
+        img: "imgs/banana.jpg",
+        price: 1,
+        company: "Metro",
+        info: 
+        "A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa.In some countries, bananas used for cooking may be called plantains, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but is usually elongated and curved, with soft flesh rich in starch covered with a rind, which may be green, yellow, red, purple, or brown when ripe. The fruits grow in clusters hanging from the top of the plant. Almost all modern edible seedless (parthenocarp) bananas come from two wild species – Musa acuminata and Musa balbisiana. The scientific names of most cultivated bananas are Musa acuminata, Musa balbisiana, and Musa × paradisiaca for the hybrid Musa acuminata × M. balbisiana, depending on their genomic constitution. The old scientific name for this hybrid, Musa sapientum, is no longer used.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Grapes",
+        img: "imgs/grapes.jpg",
+        price: 3,
+        company: "India-Mart",
+        info: 
+        "A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis.Grapes can be eaten fresh as table grapes or they can be used for making wine, jam, grape juice, jelly, grape seed extract, raisins, vinegar, and grape seed oil. Grapes are a non-climacteric type of fruit, generally occurring in clusters.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Cherry",
+        img: "imgs/cherry.jpg",
+        price: 10,
+        company: "Kompass",
+        info: 
+        "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit).Commercial cherries are obtained from cultivars of several species, such as the sweet Prunus avium and the sour Prunus cerasus. The name 'cherry' also refers to the cherry tree and its wood, and is sometimes applied to almonds and visually similar flowering trees in the genus Prunus, as in ornamental cherry or cherry blossom. Wild cherry may refer to any of the cherry species growing outside cultivation, although Prunus avium is often referred to specifically by the name wild cherry in the British Isles.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Dragon-Fruit",
+        img: "imgs/dragon-fruit.jpg",
+        price: 15,
+        company: "India-Mart",
+        info: 
+        "A pitaya or pitahaya is the fruit of several different cactus species indigenous to the Americas.Pitaya usually refers to fruit of the genus Stenocereus, while pitahaya or dragon fruit refers to fruit of the genus Hylocereus,[citation needed] both in the family Cactaceae. Dragon fruit is cultivated in Southeast Asia, India, United States, the Caribbean, Australia, Mesoamerica and throughout tropical and subtropical regions of the world.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 9,
+        title: "Papaya",
+        img: "imgs/papaya.jpg",
+        price: 4,
+        company: "Metro",
+        info: 
+        "The papaya (from Carib via Spanish), papaw, or pawpaw is the plant Carica papaya, one of the 22 accepted species in the genus Carica of the family Caricaceae.Its origin is in the tropics of the Americas, perhaps from Central America and southern Mexico.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Melon",
+        img: "imgs/melon.jpg",
+        price: 3,
+        company: "India-Mart",
+        info: 
+        "A melon is any of various plants of the family Cucurbitaceae with sweet, edible, and fleshy fruit. The word melon can refer to either the plant or specifically to the fruit. Botanically, a melon is a kind of berry, specifically a pepo. The word melon derives from Latin melopepo, which is the latinization of the Greek μηλοπέπων (mēlopepōn), meaning melon, itself a compound of μῆλον (mēlon), apple, treefruit (of any kind) and πέπων (pepōn), amongst others a kind of gourd or melon. Many different cultivars have been produced, particularly of cantaloupes.",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
+        id: 1,
+        title: "Apple",
+        img: "imgs/apple.png",
+        price: 2,
+        company: "India-Mart",
+        info: 
+        "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.",
+        inCart: false,
+        count: 0,
+        total: 0
+};
